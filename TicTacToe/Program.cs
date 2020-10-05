6d6aa6bd78b7c4dcd1f877ff0911e90845522c5a
Mathura <mathuradas333@gmail.com>
2020-10-05 14:22:26 +0530

    [Mathura]Add. CreateBoard method

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+        }
+    }
+    class TicTacToeGame
+    {
+        public char[] board = new char[10];
+        public void Board()
+        {
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = '';
+            }
+        }
+    }
+}
+
